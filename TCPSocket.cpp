63a7d62d9dc02d083ad6381be08fee2a9fbebca2
ChanhyeokYu <whd6428@gmail.com>
2025-04-21 23:57:01 +0900

    Socket

--- TCPSocket.cpp
+++ TCPSocket.cpp
@@ -25,7 +25,7 @@ bool TCPSocket::Initialize()
 
 void TCPSocket::Shutdown()
 {
-    if (Socket_ != INVALID_SOCKET)
+    if (Socket_ != INVALID_SOCKET) // 소켓이 유효할 경우 소켓을 닫기
     {
         closesocket(Socket_);
         Socket_ = INVALID_SOCKET;
@@ -37,6 +37,7 @@ bool TCPSocket::BindAndListen(int port)
 {
     isServer_ = true;
 
+    // 소켓 생성
     Socket_ = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (Socket_ == INVALID_SOCKET)
     {
@@ -44,11 +45,14 @@ bool TCPSocket::BindAndListen(int port)
         return false;
     }
 
-    sockaddr_in service;
+    // IP4용 소켓 정보를 저장
+    sockaddr_in service; 
     service.sin_family = AF_INET;
     service.sin_addr.s_addr = INADDR_ANY;
-    service.sin_port = htons(port);
+    service.sin_port = htons(port); // PC는 리틀엔디안을 사용 하지만 네트워크는 빅 엔디안을 사용하기 때문에 바이트 순서 변환
 
+    // 범용 소켓 클래스로 캐스팅 후 바인딩
+    // 나의 소켓을 쓸 객체를 바인딩
     if (bind(Socket_, (SOCKADDR*)&service, sizeof(service)) == SOCKET_ERROR)
     {
         std::cerr << "Bind failed. Error: " << WSAGetLastError() << std::endl;
@@ -57,6 +61,7 @@ bool TCPSocket::BindAndListen(int port)
         return false;   
     }
 
+    //바인딩 한 소켓 리슨
     if (listen(Socket_, SOMAXCONN) == SOCKET_ERROR)
     {
         std::cerr << "Listen failed. Error: " << WSAGetLastError() << std::endl;
@@ -71,20 +76,87 @@ bool TCPSocket::BindAndListen(int port)
 
 std::shared_ptr<TCPSocket> TCPSocket::Accpet()
 {
-    return std::shared_ptr<TCPSocket>();
+    if (!isServer_ || Socket_ == INVALID_SOCKET)
+    {
+        return nullptr;
+    }
+
+    // 커넥트를 허용한 클라이언트 소켓
+    SOCKET clientSocket = accept(Socket_, nullptr, nullptr);
+    if (clientSocket == INVALID_SOCKET)
+    {
+        std::cerr << "Accept failed. Error: " << WSAGetLastError() << std::endl; 
+        return nullptr;
+
+    }
+
+    auto client = std::make_shared<TCPSocket>();
+    client->Socket_ = clientSocket;
+    return client;
 }
 
 bool TCPSocket::Connect(const std::string& address, int port)
 {
-    return false;
+    isServer_ = false;
+
+    // 클라이언트가 사용할 소켓 생성
+    Socket_ = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (Socket_ == INVALID_SOCKET)
+    {
+        std::cerr << "Socket creation failed. Error: " << WSAGetLastError() << std::endl;
+        return false;
+    }
+
+    // IP4 구조체에 정보 입력
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    inet_pton(AF_INET, address.c_str(), &serverAddr.sin_addr); //사람이 읽기 편한 문자열을 컴퓨터가 사용하는 바이너리 문자열로 변환
+    serverAddr.sin_port = htons(port);
+
+    int result = connect(Socket_, (sockaddr*)&serverAddr, sizeof(serverAddr));
+    if (result == SOCKET_ERROR)
+    {
+        std::cerr << "Connection failed. Error: " << WSAGetLastError() << std::endl;
+        closesocket(Socket_);
+        Socket_ = INVALID_SOCKET;
+        return false;
+    }
+
+    std::cout << "Connection to server: " << address << ":" << port << std::endl;
+    return true;
+
 }
 
 int TCPSocket::Send(const char* data, int length)
 {
-    return 0;
+    if (Socket_ == INVALID_SOCKET)
+    {
+        return -1;
+    }
+    int bytesSent = send(Socket_, data, length, 0);
+    if (bytesSent == SOCKET_ERROR)
+    {
+        std::cerr << "Send failed. Error: " << WSAGetLastError() << std::endl;
+        return -1;
+    }
+    return bytesSent;
+
 }
 
 int TCPSocket::Receive(char* buffer, int length)
 {
-    return 0;
+    if (Socket_ == INVALID_SOCKET)
+    {
+        return -1;
+    }
+    
+    int bytesReceived = recv(Socket_, buffer, length, 0);
+    if (bytesReceived == SOCKET_ERROR)
+    {
+        std::cerr << "Receive fail. Error: " << WSAGetLastError() << std::endl;
+        return -1;
+    }
+
+    return bytesReceived;
+
 }
